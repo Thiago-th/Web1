18994b67631f3bc58dd7a05a291c078f26bc9bcb
Thiago-th <thiagomatheus741@gmail.com>
2025-05-21 11:50:10 -0300

    Primeiro commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+alert("Bem vindo ao curso!");
+/*
+document.getElementById("nome").value = "Oi";
+Busca no DOM os elementos da página criados usando sua Id
+.value altera o valor do elemento pela string entre "aspas"
+*/
